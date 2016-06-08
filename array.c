224495ee0c7533d7c31fdbdb19ff68ba7664a265
shalinidharshu <babyshalini.cs13@bitsathy.ac.in>
2016-06-08 11:13:04 +0530

    Update array.c

--- array.c
+++ array.c
@@ -2,7 +2,12 @@
 #include<stdio.h>
 int main()
 {
-int a[5]={1,1,2,3,3},i,c=0,j;
+int a[15],i,c=0,j;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
 for(i=0;i<5;i++)
 {
     for(j=0;j<5;j++)
