6dfeeaf44fde5ebd5ac945c41835ae546e57235d
shalinidharshu <babyshalini.cs13@bitsathy.ac.in>
2016-06-08 10:16:25 +0530

    reverse and vowel remove

---
+++ reversevowel.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[20]="shalini",b[20];
+    int l,i;
+    l=strlen(a);
+    for(i=l-1;i>=0;i--)
+    {
+        b[i]=a[i];
+        printf("%c",b[i]);
+    }
+printf("\n");
+    for(i=l-1;i>=0;i--)
+    {
+    if(b[i]!='a'&&b[i]!='A'&&b[i]!='e'&&b[i]!='E'&&b[i]!='i'&&b[i]!='I'&&b[i]!='o'&&b[i]!='O'&&b[i]!='u'&&b[i]!='U')
+    {
+        printf("%c",b[i]);
+    }
+}
+
+return 0;
+}
